3a23ff9ecde813782d880a778ddc7bea5b7edce6
Marilea Minasi <marileaminasi@MBP-di-Marilea.lan>
2022-09-03 17:05:10 +0200

    added projects

--- src/static/data/progetti.ts
+++ src/static/data/progetti.ts
@@ -4,16 +4,26 @@ const PROJECTS: Record<
   ProjectType,
   Array<{ filename: string; alt: string }>
 > = {
-  // QUA
+  // LE IMMAGINI DEVI METTERLA NELLA CARTELLA PUBLIC/IMAGES/ !!!
   // ----------------------------------------
   "web-design": [
-    { filename: "events-site.png", alt: "Web design 1" },
-    { filename: "shopisea.png", alt: "Web design 2" },
-    { filename: "sito-carmine.png", alt: "Web design 3" },
+    { filename: "events-site.webp", alt: "landing page sito di eventi" },
+    { filename: "shopisea.webp", alt: "landing page servizi nft e metaverso" },
+    { filename: "sito-carmine.webp", alt: "landing page portfolio programmatore" },
   ],
-  "app-design": [],
-  "graphic-design": [],
-  "social-design": [],
+  "app-design": [
+    { filename: "fresh-fleur.webp", alt: "interfaccia app negozio piante" },
+    { filename: "big-hack.webp", alt: "interfaccia app finalisti hackaton 2022" },
+  ],
+  "graphic-design": [
+    { filename: "logo-carmine.webp", alt: "logo programmatore" },
+    { filename: "studio-bimbi.webp", alt: "logo asilo" },
+  ],
+  "social-design": [
+    { filename: "beauty.webp", alt: "post social media compagnia bellezza" },
+    { filename: "ig-work.webp", alt: "post social media abbigliamento" },
+    { filename: "instagram-post.webp", alt: "post social media negozio articoli da regalo" },
+  ],
   // ----------------------------------------
 }
 
