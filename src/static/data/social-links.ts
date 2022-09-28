74bf41df078141ce04bf463ac085728b1a5deca6
Carmine Porricelli <carmineporricelli96@gmail.com>
2022-09-28 17:10:27 +0200

    Changed Dribbble link to Behance link

--- src/static/data/social-links.ts
+++ src/static/data/social-links.ts
@@ -2,6 +2,7 @@ const SOCIAL_LINKS = {
   LINKEDIN: "https://www.linkedin.com/in/marileaminasi",
   DRIBBBLE: "https://dribbble.com/minasi",
   FIVERR: "https://www.fiverr.com/holycannolo",
+  BEHANCE: "https://www.behance.net/marileaminasi",
 }
 
 export default SOCIAL_LINKS
